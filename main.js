8c0e9a29dfb2470d158a4ff61f333dd1b1a06a5c
Xavier Martinez <luis.martz7@hotmail.com>
2019-08-27 21:14:03 -0500

    creacion de metodos constructores para crear el board del juego, y los jugadores, hasta ahora ya se pueden mover, y falta agregar las funciones para disparar, y el score de los jugadores

--- main.js
+++ main.js
@@ -4,28 +4,57 @@ const ctx = canvas.getContext('2d')
 let frames = 0
 let interval
 
-
-function gameboard () {
-    this.width = canvas.width;
-    this.heigth = canvas.heigth;
-    this.x = 0;
-    this.y = 0;
-    this.img = new Image();
-    this.img.src = './img/landing_game.png';
-    this.img.onload = () => {
-        ctx.drawImage(this.img, this.x, this.y, this.width, this.height);
+class Gameboard {
+    constructor() {
+        this.width = canvas.width;
+        this.height = canvas.height;
+        this.x = 0;
+        this.y = 0;
+        this.img = new Image();
+        this.img.src = './img/fondo.png';
     }
-    this.draw = () => {
-        ctx.drawImage(this.img, this.x, this.y, this.width, this.height)
+    draw() {
+        ctx.drawImage(this.img, this.x, this.y, this.width, this.height);
+    };
+    style() {
+        ctx.fillRect(0, 0, canvas.width, canvas.height)
+        ctx.fillStyle
     }
 }
 
-const board = new gameboard()
+const board = new Gameboard()
+
+class Player {
+    constructor(img, width, height, x, y, status) {
+        this.img = new Image()
+        this.img.src = img
+        this.width = width
+        this.height = height
+        this.x = x
+        this.y = y
+        this.speedX = 0
+        this.status = status
+    }
+    draw() {
+        ctx.drawImage(this.img, this.x, this.y, this.width, this.height);
+    }
+
+    newPos() {
+        this.x += this.speedX
+    }
+
+    updateStatus(status) {
+        this.status = status
+    }
+}
+
+let player1 = new Player('./img/at-at_P1.png', 200, 160, 0, 420, true)
+let player2 = new Player('./img/at-at_P2.png', 200, 160, 1100, 420, false)
 
 
 const startButton = document.querySelector('#start')
 startButton.onclick = () => {
-    console.log('game started')
+    console.log('Mi juego esta VIVOOO')
     startGame()
 }
 
@@ -36,21 +65,55 @@ const startGame = () => {
 function updateCanvas() {
     frames += 1
     ctx.clearRect(0, 0, canvas.width, canvas.height)
+    player1.newPos()
+    player2.newPos()
     board.draw()
+    player1.draw()
+    player2.draw()
 }
 
 
-// class player {
-//     constructor(width, heigth, x, y) {
-//         this.width = width
-//         this.heigth = heigth
-//         this.x = x
-//         this.y = y
-//         this.posX = 0
-//         this.posY = 0
-//         this.img = Image()
-//         this.img.src = './img/at-atP1.png'
-//         this.img.onload = () => {
-//             ctx.drawImage(this.img, this.width, this.height, this.x, this.y)
-//         }
-//     }
+document.onkeydown = e => {
+    if (player1.status == true) {
+        switch (e.keyCode) {
+            case 65:
+                if (player1.x <= 0)
+                    player1.x = 0
+                else
+                    player1.speedX -= 1
+                break
+            case 68:
+                if (player1.x >= 4 * (canvas.width) / 9 - (player1.width))
+                    player1.x = 4 * (canvas.width) / 9 - (player1.width)
+                else
+                    player1.speedX += 1
+                break
+            default:
+                break
+        }
+    }
+    if (player2.status == true) {
+        switch (e.keyCode) {
+            case 74:
+                if(player2.x <= 5 * (canvas.width) / 9)
+                player2.x = 5 * (canvas.width) / 9
+                else
+                player2.speedX -= 1
+                break
+
+            case 76:
+                if ((player2.x + player2.width) >= canvas.width)
+                    player2.x = canvas.width - player2.width
+                else
+                    player2.speedX += 1
+            default:
+                break
+
+        }
+    }
+}
+
+document.onkeyup = e => {
+    player1.speedX = 0
+    player2.speedX = 0
+}
